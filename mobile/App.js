0cea6041f45294f94a22193c7d01bc04a097affd
unknown <tce@NOTE-LEN024.tce.go.gov.br>
2020-03-28 02:20:05 -0300

    Mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'intl/locale-data/json/pt-BR';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+          <Routes/>
+  );
+}
+
+
